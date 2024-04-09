b57efb4a5e75d4754eff0611a6e743d8e08bf3f9
ingenium11 <140139886+ingenium11@users.noreply.github.com>
2024-04-09 19:36:06 +0900

    Update login.php

--- login.php
+++ login.php
@@ -3,20 +3,21 @@
   $pw = $_GET['pw_param'];
 
   $db_conn = mysqli_connect("127.0.0.1", "webhacking_db", "webhacking", "login");
-  if($db_conn == false) {
+  if($db_conn == false){
     echo mysqli_connect_error();
   }
+
   else {
     $query = "select * from user where id='{$id}' and pw='{$pw}'";
     $result = mysqli_query($db_conn, $query);
-    echo "query : {$query} <br>";
+    echo "query : {$query}<br>";
 
-    if ($result == false) {
+    if($result == false) {
       echo mysqli_error($db_conn);
     }
     else {
       $row = mysqli_fetch_array($result);
-      if ($row) {
+      if($row) {
         echo "Hello {$row['id']}, login success!";
       }
       else {
